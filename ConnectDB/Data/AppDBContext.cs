abe24e48f47d496eaf41a7e3e1bba1487a21fb53
Admin <Admin@D20930>
2026-03-20 14:52:33 +0700

    Add project files.

---
+++ ConnectDB/Data/AppDBContext.cs
@@ -0,0 +1,10 @@
+﻿using ConnectDB.Modles;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConnectDB.Data;
+public class AppDbContext : DbContext
+{
+    // Constructor này bắt buộc phải có để nhận Connection String từ Program.cs
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<Student> Students { get; set; }
+}
