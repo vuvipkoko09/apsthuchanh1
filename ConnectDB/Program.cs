1a0aa242afbe9beeafabd6fb129121ceb8acb252
Vu Nguyen <vu.nguyen@example.com>
2026-04-23 23:30:26 +0700

    deloy lên render

--- ConnectDB/Program.cs
+++ ConnectDB/Program.cs
@@ -120,5 +120,5 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
-
-app.Run();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Run($"http://0.0.0.0:{port}");
